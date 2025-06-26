1a2df8f993eb9b6947038afac137611b815046a6
Stephanie Gredell <s.raide@gmail.com>
2025-06-26 08:03:26 -0700

    Initial Commit

---
+++ Retail-Convenience/Retail_ConvenienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Retail_ConvenienceApp.swift
+//  Retail-Convenience
+//
+//  Created by Stephanie Gredell on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Retail_ConvenienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
